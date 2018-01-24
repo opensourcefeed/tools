ca7ddd75b65dd62ac2a701d2ed4f302f43322634
OpenSourceFeeder <opensourcefeeder@gmail.com>
2018-01-25 00:49:31 +0530

    Add time zones & epoch unit

--- epochconvertor/script/app.js
+++ epochconvertor/script/app.js
@@ -1,10 +1,14 @@
 var app = angular.module("app", []);
 
 app.controller("EpochController", function($scope, $timeout) {
-	
+	$scope.unit = 1;
+	$scope.zone = "+8";
+
 	$scope.convert = function() {
+
 		var epoch = $scope.epoch;
-		var date = new Date(epoch);
+		var unit = $scope.unit;
+		var date = new Date(epoch * unit);
 		$scope.showTime = date != 'Invalid Date' && $scope.epoch != null;
 	};
 	var tick = function() {
